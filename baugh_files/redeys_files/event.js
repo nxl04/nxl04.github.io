afb904567e29a67f55852777940d9f8b57269c75
nxl04 <thedarkworld2904@gmail.com>
2025-11-01 12:10:27 +0700

    Add files via upload

---
+++ baugh_files/redeys_files/event.js
@@ -0,0 +1,9 @@
+﻿/*<!--*/var cspDomains="undefined"!=typeof ["criteo.com","criteo.net","crto.in","localhost","127.0.0.1","hlserve.com","ctolabperfstats.com"]?["criteo.com","criteo.net","crto.in","localhost","127.0.0.1","hlserve.com","ctolabperfstats.com"]:[];window.cto_csm_CspLogger&&"function"==typeof window.cto_csm_CspLogger.addAllowedDomains&&window.cto_csm_CspLogger.addAllowedDomains(cspDomains);try { (function() {
+        var sendGetHttpRequestThroughImg = function(aurl) {
+            var pixelRequest = new Image();
+            pixelRequest.src = aurl;
+        }
+    sendGetHttpRequestThroughImg('https://www.facebook.com/tr?id=784777860531505&ev=PageView&cd[content_type]=product&cd[content_ids]=%5B%22%22%5D&eid=1d117b5760e84fb8a5be6a0cb1e46e46&cd[application_id]=423936147658676&fbp=fb.1.1761972842000.1111735115&cd[p_id]=111036&cd[is_app_event]=n&dl=https%3A%2F%2Fwww.overstock.com%2F%3Fcid%3D341089%26utm_source%3Dadmarketplace%26utm_medium%3Dpkwcpc%26utm_campaign%3DUS_Overstock_NonBrand%26utm_content%3De242863947505471488%26mfadid%3Dadm');
+})();
+ } catch (e) { console.debug(e); }
+/*-->*/
